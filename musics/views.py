9c4a80dd2815b1f07abc8e8e53c3eb296c320a2c
oliviaspark0825 <suhyunpark0825@gmail.com>
2019-04-23 06:35:57 +0000

    02_artist list && comment create

--- musics/views.py
+++ musics/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import MusicSerializer
+from .serializers import MusicSerializer, ArtistSerializer, ArtistDetailSerializer, CommentSerializer
 
-from .models import Music
+from .models import Music, Artist
 # Create your views here.
 
 
@@ -20,3 +20,30 @@ def music_list(request):
 #응답하는 함수는 Response이다
 #결과로 보내줄 데이터는 .data로 가져온다
 
+@api_view(['GET'])
+def music_detail(request, music_pk):
+    music = get_object_or_404(Music, pk=music_pk)
+    serializer = MusicSerializer(music)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def artist_list(request):
+    artists = Artist.objects.all()
+    serializer = ArtistSerializer(artists, many=True)
+    return Response(serializer.data)
+    
+@api_view(['GET'])
+def artist_detail(request, artist_pk):
+    artist = get_object_or_404(Artist, pk=artist_pk)
+    serializer = ArtistDetailSerializer(artist)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def comment_create(request, music_pk): # 몇번 음악에 달리는 댓글인지
+    serializer = CommentSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save(music_id=music_pk)
+        return Response(serializer.data)
+        
+        
